8edf3611711a26b0a8abe0ca75b204262b5e25b6
ottalwal <142910148+ottalwal@users.noreply.github.com>
2024-05-01 10:44:31 -0400

    5/1/24

--- js/javascriptb.js
+++ js/javascriptb.js
@@ -1,10 +1,13 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar_menu');
-const navLogo = document.querySelector('#navbar_logo');
-// Display Mobile Menu
-const mobileMenu = () => {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
-};
+window.addEventListener("DOMContentLoaded", (event) => {
+  const menu = document.querySelector('#mobile_menu');
+  const menuLinks = document.querySelector('.navbar_menu');
+  const navLogo = document.querySelector('#navbar_logo');
+  // Display Mobile Menu
+  const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+  };
 
-menu.addEventListener('click', mobileMenu);
+  menu.addEventListener('click', mobileMenu);
+});
+
